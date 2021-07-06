5140e2bd7b3509cc2d21424af3526f3d4d9bfd30
kr8n3r <jani@kraner.dev>
2021-07-06 18:16:59 +0100

    Remove Raise Hand button and type from  message queue

--- src/components/messages/MessageWrapper.vue
+++ src/components/messages/MessageWrapper.vue
@@ -2,7 +2,6 @@
   <div>
     <transition-group name="fading" class="nod-message-wrapper">
       <Message v-for="msg in messages" :key="msg.messageId" :encoded="msg.encoded" :username="msg.username" :img="msg.img" />
-      <Hand v-for="hand in hands" :key="hand.messageId" :username="hand.username" :img="hand.img" :messageId="hand.messageId" />
       <Response v-for="response in responses" :key="response.messageId" :username="response.username" :img="response.img" :messageId="response.messageId" />
     </transition-group>
   </div>
@@ -10,14 +9,12 @@
 
 <script>
 import Message from "./Message";
-import Hand from "./Speak";
 import Response from "./Respond";
 import { generateUUID, sendNotification } from "../../utils";
 
 export default {
   components: {
     Message,
-    Hand,
     Response
   },
   computed: {
@@ -46,28 +43,6 @@ export default {
           });
           break;
           
-        case "QUEUEHAND":
-          this.$store.dispatch("addHand", {
-            messageId: d.message.messageId,
-            username: d.message.username,
-            encoded: d.message.encoded,
-            img: d.message.img,
-            owner: false
-          });
-
-          if (this.$store.state.visible === false && localStorage.getItem("notificationStatus") === "true") {
-            chrome.runtime.sendMessage(this.$store.state.extensionID, {
-              type: "displayNotification",
-              options: {
-                title: "Notification from Hand signals",
-                message: d.message.username,
-                iconUrl: d.message.encoded,
-                type: "basic"
-              }
-            });
-          }
-          break;
-
         case "RESPONDQUEUE":
           this.$store.dispatch("addRespond", {
             messageId: d.message.messageId,
@@ -88,9 +63,6 @@ export default {
               }
             });
           }
-          break;
-        case "REMOVEHAND":
-          this.$store.dispatch("removeHand", d.message.messageId);
           break;
         case "REMOVERESPONSE":
           this.$store.dispatch("removeRespond", d.message.messageId);
@@ -118,4 +90,5 @@ export default {
   flex-direction: column;
   align-items: flex-start;
 }
+
 </style>
