9b7344e59b28d61a0a43cb6423f361363f82a824
kr8n3r <jani@kraner.dev>
2021-07-14 18:04:15 +0100

    Add user setting to show/ hide the Raise hand signal

---
+++ src/components/settings/SetRaiseHandVisibility.vue
@@ -0,0 +1,23 @@
+<template>
+  <label class="dropdown-item">
+    <input type="checkbox" v-model="isVisible" @change="saveState(isVisible)"  />
+      Show Raise Hand signal
+  </label>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        isVisible: false,
+      };
+    },
+    methods: {
+      saveState(value){
+        this.$store.dispatch("setRaiseHandVisibility", value);
+      },
+    },
+    created() {
+      this.isVisible = this.$store.state.isRaiseHandVisible;
+    }
+  };
+</script>
